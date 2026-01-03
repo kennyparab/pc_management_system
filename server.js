41728999f4ea669191af01e2de622995872c514a
omparab <pom23cs@student.mes.ac.in>
2026-01-03 19:10:28 +0530

    Fourth Commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Use DATABASE_URL
 const pool = new Pool({
